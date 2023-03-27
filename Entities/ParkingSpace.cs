9c166813f75e9a331afad1152f7eb95f7f0f3763
yuridsm <yuri.melo@jurify.com.br>
2023-03-27 14:22:16 -0300

    new modifications

--- Entities/ParkingSpace.cs
+++ Entities/ParkingSpace.cs
@@ -5,7 +5,6 @@ namespace Test.Entities
     public abstract class ParkingSpace : IEntity
     {
         public Guid Identifier { get; } = Guid.NewGuid();
-
         public ParkingSpaceType Type { get; protected set; }
         public ParkingSpaceStatus Status { get; protected set; }
     }
